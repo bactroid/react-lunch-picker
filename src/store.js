132cdeebef9450e55ed4af32369b0af6855d9b21
Kate Haskell <fuzzcat@bactroid.net>
2018-10-23 05:44:44 -0400

    Replaced redux-promise with redux-saga

--- src/store.js
+++ src/store.js
@@ -1,11 +1,16 @@
 import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './reducers'
-import ReduxPromise from 'redux-promise'
+import createSagaMiddleware from 'redux-saga'
+
+const sagaMiddleware = createSagaMiddleware()
 
 // When you create your Redux store, you can optionally apply
 // middleware. Here, we're applying ReduxPromise, which intercepts
 // actions that have promises in them and resolves them on our behalf.
 
 export default () => {
-  return createStore(rootReducer, applyMiddleware(ReduxPromise))
+  return createStore(
+    rootReducer,
+    applyMiddleware(sagaMiddleware)
+  )
 }
