ccec7fab3f20947622e282f3b116183c57cf98a0
Kate Haskell <khaskell@ufl.edu>
2018-10-23 08:38:31 -0400

    Add comments and remove old code

--- src/actions/restaurant.js
+++ src/actions/restaurant.js
@@ -1,14 +1,14 @@
-// import axios from 'axios'
-
-// const LUNCH_PICKER_URL = 'https://6zkojio7gj.execute-api.us-east-1.amazonaws.com/dev/lunch'
-
 // Use constants because it saves us from typing string wrong.
 // Sidenote: This is a kludge to work around not having algebraic
 // data types (like Elm).
+
 export const RESTAURANT_UPDATE = 'RESTAURANT_UPDATE'
 export const RESTAURANT_LOADING = 'RESTAURANT_LOADING'
 export const RESTAURANT_ERROR = 'RESTAURANT_ERROR'
 export const RESTAURANT_FETCH = 'RESTAURANT_FETCH'
+
+// This action creator will get mapped onto our App component via
+// `mapDispatchToProps`.
 
 export const fetchRestaurant = () => ({
   type: RESTAURANT_FETCH
--- src/sagas/index.js
+++ src/sagas/index.js
@@ -11,6 +11,16 @@ import {
 const LUNCH_PICKER_URL =
   'https://6zkojio7gj.execute-api.us-east-1.amazonaws.com/dev/lunch'
 
+// Every time you see `yield` in this code, we're either resolving a
+// promise or sending an action over to Redux.
+
+// Yield Calls
+// 1. Tell Redux we're loading.
+// 2. Call our API using axios and wait on the promise resolution.
+// 3. If we got a successful response, tell Redux to update our
+//    restaurant.
+// 4. If we got an error, tell Redux about it.
+
 export function* fetchRestaurant () {
   yield put({ type: RESTAURANT_LOADING })
   try {
@@ -20,6 +30,11 @@ export function* fetchRestaurant () {
     put({ type: RESTAURANT_ERROR })
   }
 }
+
+// This saga is "listening" in the background for when the
+// RESTAURANT_FETCH call is made. This is what `takeEvery` does. When
+// it sees our RESTAURANT_FETCH action, it calls the fetchRestaurant
+// saga.
 
 export function* watchFetchRestaurant () {
   yield takeEvery(RESTAURANT_FETCH, fetchRestaurant)
