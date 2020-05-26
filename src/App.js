22c6ee0b1e184440fdfb36751c01e31d2b6e4c60
Kate Haskell <fuzzcat@bactroid.net>
2020-05-26 14:41:17 -0400

    Switched to very basic React hooks implementation

--- src/App.js
+++ src/App.js
@@ -1,65 +1,30 @@
-import React, { Component } from 'react'
-// import React from 'react'
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
+import React, { useEffect, useState } from 'react'
+import {get} from 'axios'
 import './App.css'
-import { fetchRestaurant } from './actions/restaurant'
 import RestaurantDisplay from './components/RestaurantDisplay'
 
-// Class-based approach
+const LUNCH_PICKER_URL = 'https://6zkojio7gj.execute-api.us-east-1.amazonaws.com/dev/lunch'
 
-class App extends Component {
-  // Kind of like onInit
-  componentDidMount () {
-    console.log('mounted!')
-    this.props.fetchRestaurant()
-  }
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true)
+  const [restaurant, setRestaurant] = useState('butts')
 
-  render () {
-    return (
-      <div className='App'>
-        <RestaurantDisplay
-          restaurant={this.props.restaurant}
-          fetchRestaurant={this.props.fetchRestaurant}
-        />
-      </div>
-    )
-  }
+  useEffect(() => {
+    get(LUNCH_PICKER_URL).then(response => {
+      setRestaurant(response.data.text)
+      setIsLoading(false)
+    })
+  }, [])
+
+
+
+  return (
+    <div className='App'>
+      <RestaurantDisplay
+        restaurant={isLoading ? 'Loading...' : restaurant}
+      />
+    </div>
+  )
 }
 
-// Kate's Preferred Less Verbose Functional Component Approach™
-
-// Can't have state in these components, however, which means all state must go
-// in Redux. This may or may not be the right call based on performance based
-// on what I'm reading, but I feel like starting this way and only
-// converting when necessary may be right?
-
-// These components don't have lifecycle methods like componentDidMount. They
-// act purely on props passed in. Now certainly those props can come from
-// Redux.
-
-// const App = props => (
-//   <div className='App'>
-//     <div>{props.restaurant}</div>
-//     <button onClick={props.fetchRestaurant}>Get Random Restaurant</button>
-//   </div>
-// )
-
-// Turns Redux state into React component props
-
-const mapStateToProps = ({ restaurant }) => ({
-  restaurant
-})
-
-// Turns Redux actions into React component props
-
-function mapDispatchToProps(dispatch) {
-  return bindActionCreators({ fetchRestaurant }, dispatch)
-}
-
-// connect is the magic that lets a React component know about Redux.
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App)
+export default App
