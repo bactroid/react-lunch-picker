bddf9bbd9b9d2e2dde408577e90a9b17c9f0fff9
Kate Haskell <fuzzcat@bactroid.net>
2020-05-27 16:26:48 -0400

    Return to a simple useState implementation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useReducer } from "react";
+import React, { useEffect, useState } from "react";
 import { get } from "axios";
 import "./App.css";
 import RestaurantDisplay from "./components/RestaurantDisplay";
@@ -6,54 +6,22 @@ import RestaurantDisplay from "./components/RestaurantDisplay";
 const LUNCH_PICKER_URL =
   "https://6zkojio7gj.execute-api.us-east-1.amazonaws.com/dev/lunch";
 
-// Actions
-const FETCH_RESTAURANT_REQUEST = "FETCH_RESTAURANT_REQUEST";
-const FETCH_RESTAURANT_SUCCESS = "FETCH_RESTAURANT_SUCCESS";
-const FETCH_RESTAURANT_ERROR = "FETCH_RESTAURANT_ERROR";
-
-// Initial App State
-const initialState = {
-  isLoading: true,
-  hasError: false,
-  restaurant: ""
-};
-
-const restaurantReducer = (state, action) => {
-  switch (action.type) {
-    case FETCH_RESTAURANT_REQUEST:
-      return {
-        ...state,
-        isLoading: true,
-        hasError: false
-      };
-    case FETCH_RESTAURANT_SUCCESS:
-      return {
-        ...state,
-        isLoading: false,
-        restaurant: action.payload
-      };
-    case FETCH_RESTAURANT_ERROR:
-      return {
-        ...state,
-        isLoading: false,
-        hasError: true
-      };
-    default:
-      return state;
-  }
-};
-
 const App = () => {
-  const [state, dispatch] = useReducer(restaurantReducer, initialState);
-  const { isLoading, restaurant } = state;
+  const [isLoading, setIsLoading] = useState(true);
+  const [hasError, setHasError] = useState(false);
+  const [restaurant, setRestaurant] = useState("");
 
   const fetchRestaurant = async () => {
-    dispatch({ type: FETCH_RESTAURANT_REQUEST });
     try {
+      setIsLoading(true);
+      setHasError(false);
       const response = await get(LUNCH_PICKER_URL);
-      dispatch({ type: FETCH_RESTAURANT_SUCCESS, payload: response.data.text });
+      setRestaurant(response.data.text);
+      setIsLoading(false);
+      setHasError(false);
     } catch (e) {
-      dispatch({ type: FETCH_RESTAURANT_ERROR, payload: e });
+      setIsLoading(false);
+      setHasError(true);
     }
   };
 
